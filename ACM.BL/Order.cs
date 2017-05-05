308d4bd297bf7183d83afc7988483e291abd1d32
ZachParris <zacharyjparris@gmail.com>
2017-05-05 17:31:39 -0500

    Moved code to repo lowering coupling

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -21,20 +21,6 @@ namespace ACM.BL
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
 
-        //Retrieve one order
-        public Order Retrieve(int orderId)
-        {
-            //retrieves the defined order
-            return new Order();
-        }
-
-        //Saves the current order
-        public bool Save()
-        {
-            //saves the defined order
-            return true;
-        }
-
         public bool Validate()
         {
             var isValid = true;
--- ACM.BL/OrderRepository.cs
+++ ACM.BL/OrderRepository.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    class OrderRepository
+    public class OrderRepository
     {
+        //Retrieve one order
+        public Order Retrieve(int orderId)
+        {
+            Order order = new Order(orderId);
+            //retrieves the defined order
+            if(orderId == 10)
+            {
+                order.OrderDate = new DateTimeOffset(2014, TimeSpan.Zero);
+            }
+
+            return order;
+        }
+
+        //Saves the current order
+        public bool Save()
+        {
+            //saves the defined order
+            return true;
+        }
+
     }
 }
